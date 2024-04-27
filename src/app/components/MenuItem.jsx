02c9b9d15693176c6c8d6081f58fdce11491131f
Valerii <valerabulon@gmail.com>
2024-04-27 19:35:37 +0300

    add header section

---
+++ src/app/components/MenuItem.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+
+export default function MenuItem({ title, address, Icon }) {
+  return (
+    <Link href={address} className="hover:text-amber-500">
+      <Icon className="text-2xl sm:hidden" />
+      <p className="uppercase hidden sm:inline text-sm">{title}</p>
+    </Link>
+  );
+}
+
+/* {
+  "tailwindCSS.includeLanguages": {
+      "javascript": "javascript",
+      "html": "HTML"
+  },
+ "editor.quickSuggestions": {
+     "strings": true
+  }
+} */
