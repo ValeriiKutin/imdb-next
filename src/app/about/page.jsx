9f7c33337e993f3d74611d8f191848c6c689483f
Valerii <valerabulon@gmail.com>
2024-04-28 12:59:20 +0300

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,40 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 gap-4 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
+        repellendus, vitae sed nostrum vel eius porro id. Aspernatur officiis
+        in, unde quas amet soluta ipsa id assumenda dolor tenetur maxime.
+        Delectus id esse voluptate perferendis facere reiciendis error illo
+        consequatur, nihil itaque asperiores earum inventore aliquam excepturi
+        eligendi quas rem nulla ipsam cupiditate dolorem aperiam odit dolorum
+        accusantium deserunt! Ipsum. Quas at consectetur ipsam vel nihil, beatae
+        iusto itaque saepe possimus praesentium consequatur recusandae provident
+        asperiores illum perspiciatis ullam reprehenderit sint vitae ipsum iure,
+        delectus facere soluta. Quibusdam, ipsam dolorem? Fugiat a harum fuga
+        laboriosam voluptate, autem ullam esse atque aspernatur consectetur
+        inventore velit dolorum accusantium dolorem, quo quas, debitis explicabo
+        enim et error facere rerum. Et architecto doloremque laudantium.
+        Possimus dignissimos quod nisi optio soluta ad sint magni, quaerat
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
+        repellendus, vitae sed nostrum vel eius porro id. Aspernatur officiis
+        in, unde quas amet soluta ipsa id assumenda dolor tenetur maxime.
+        Delectus id esse voluptate perferendis facere reiciendis error illo
+        consequatur, nihil itaque asperiores earum inventore aliquam excepturi
+        eligendi quas rem nulla ipsam cupiditate dolorem aperiam odit dolorum
+        accusantium deserunt! Ipsum. Quas at consectetur ipsam vel nihil, beatae
+        iusto itaque saepe possimus praesentium consequatur recusandae provident
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
+        repellendus, vitae sed nostrum vel eius porro id. Aspernatur officiis
+        in, unde quas amet soluta ipsa id assumenda dolor tenetur maxime.
+        Delectus id esse voluptate perferendis facere reiciendis error illo
+        consequatur, nihil itaque asperiores earum inventore aliquam excepturi
+      </p>
+    </div>
+  );
+}
